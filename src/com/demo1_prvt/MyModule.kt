2993dc3bb67553213f6ae7015062db3dc245df66
pk <pk@varan.spb.ru>
2020-10-23 23:43:24 +0300

    view1: shipping_order join customer on customer id

--- src/com/demo1_prvt/MyModule.kt
+++ src/com/demo1_prvt/MyModule.kt
@@ -1,5 +1,6 @@
 package com.demo1_prvt
 
+import com.dolmen.md.demo1_prvt.Customer
 import com.dolmen.md.demo1_prvt.Demo1_PrvtModuleBase
 import com.dolmen.md.demo1_prvt.Shipping_Order
 import com.dolmen.md.demo1_prvt.View1
@@ -17,13 +18,15 @@ class MyModule : Demo1_PrvtModuleBase() {
         class ViewIterator(f: Formula, m: MyModule): View1.Data(f, m) {
             override fun create(s: Shipping_Order): View1 {
                 val v = super.create(s)
+                if (s.customer != null) {
+                    val c = select(Customer(), s.customer)
 
-                v.c_Phone = "ppp"
-                v.c_Mobile = "m"
-                v.c_Address_Line1 = "aline1"
-                v.c_Address_Line2 = "aline2"
-                v.c_Address_Line3 = "aline3"
-
+                    v.c_Phone = c.phone
+                    v.c_Mobile = c.mobile
+                    v.c_Address_Line1 = c.address_Line1
+                    v.c_Address_Line2 = c.address_Line2
+                    v.c_Address_Line3 = c.address_Line3
+                }
                 return v
             }
         }
