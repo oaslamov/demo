3f580db0395673c190fa1b97a0a9c4c239bfb618
pk <pk@varan.spb.ru>
2020-10-24 20:23:58 +0300

    Added * price() for shipping_order_product * sum() for shipping_order_product * total() for shipping_order

--- src/com/demo1_prvt/MyModule.kt
+++ src/com/demo1_prvt/MyModule.kt
@@ -15,28 +15,37 @@ class MyModule : Demo1_PrvtModuleBase() {
         return Text.F("\n-------------\nTest OK (input: $0)\n--------------", input)
     }
 
+    private fun Shipping_Order_Product.sum(): BigDecimal {
+        return (this.price() * BigDecimal(this.quantity)).setScale(2, RoundingMode.HALF_UP)
+    }
+
+    private fun Shipping_Order_Product.price(): BigDecimal {
+        val p = selectFirst<Product>("id = ${this.product}")
+        if (p != null) {
+            return BigDecimal(p.price).setScale(2, RoundingMode.HALF_UP)
+        } else return BigDecimal.ZERO
+    }
+
+    private fun Shipping_Order.total(): BigDecimal {
+        var sum: BigDecimal = BigDecimal.ZERO
+        iterate<Shipping_Order_Product>("shipping_order = ${this.id}") { item ->
+            sum += item.sum()
+        }
+        return sum
+    }
 
     override fun s_iterateView1(f: Formula): SelectedData<View1> {
         class ViewIterator(f: Formula, m: MyModule) : View1.Data(f, m) {
             override fun create(s: Shipping_Order): View1 {
                 val v = super.create(s)
+                v.total_Sum = s.total()
                 if (s.customer != null) {
                     val c = select(Customer(), s.customer)
-                    var sum = 0.00
-
-                    iterate<Shipping_Order_Product>("shipping_order = ${s.id}") { item ->
-                        val p = selectFirst<Product>("id = ${item.product}")
-                        if (p != null) {
-                            sum += item.quantity * p.price
-                        }
-                    }
-
                     v.c_Phone = c.phone
                     v.c_Mobile = c.mobile
                     v.c_Address_Line1 = c.address_Line1
                     v.c_Address_Line2 = c.address_Line2
                     v.c_Address_Line3 = c.address_Line3
-                    v.total_Sum = BigDecimal(sum).setScale(2, RoundingMode.HALF_UP)
                 }
                 return v
             }
@@ -48,11 +57,8 @@ class MyModule : Demo1_PrvtModuleBase() {
         class ViewIterator(f: Formula, m: MyModule) : View2.Data(f, m) {
             override fun create(s: Shipping_Order_Product): View2 {
                 val v = super.create(s)
-                val p = selectFirst<Product>("id = ${s.product}")
-                if (p != null) {
-                    v.price = BigDecimal(p.price).setScale(2, RoundingMode.HALF_UP)
-                    v.sum = BigDecimal(s.quantity) * v.price
-                }
+                v.price = s.price()
+                v.sum = s.sum()
                 return v
             }
         }
