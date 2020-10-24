dfd6c5660a4a2fa41916ae2ec2608275df2cedd9
pk <pk@varan.spb.ru>
2020-10-24 13:59:17 +0300

    Added order total calculations to view1

--- src/com/demo1_prvt/MyModule.kt
+++ src/com/demo1_prvt/MyModule.kt
@@ -1,31 +1,40 @@
 package com.demo1_prvt
 
-import com.dolmen.md.demo1_prvt.Customer
 import com.dolmen.md.demo1_prvt.Demo1_PrvtModuleBase
-import com.dolmen.md.demo1_prvt.Shipping_Order
-import com.dolmen.md.demo1_prvt.View1
+import com.dolmen.md.demo1_prvt.*
+
 import com.dolmen.serv.conn.SelectedData
 import com.dolmen.serv.exp.Formula
 import com.dolmen.util.Text
 
 class MyModule : Demo1_PrvtModuleBase() {
 
-    fun myAction(input: String): String{
+    fun myAction(input: String): String {
         return Text.F("\n-------------\nTest OK (input: $0)\n--------------", input)
     }
 
+
     override fun s_iterateView1(f: Formula): SelectedData<View1> {
-        class ViewIterator(f: Formula, m: MyModule): View1.Data(f, m) {
+        class ViewIterator(f: Formula, m: MyModule) : View1.Data(f, m) {
             override fun create(s: Shipping_Order): View1 {
                 val v = super.create(s)
                 if (s.customer != null) {
                     val c = select(Customer(), s.customer)
+                    var sum = 0.00
+
+                    iterate<Shipping_Order_Product>("shipping_order = ${s.id}") { item ->
+                        val p = selectFirst<Product>("id = ${item.product}")
+                        if (p != null) {
+                            sum += item.quantity * p.price
+                        }
+                    }
 
                     v.c_Phone = c.phone
                     v.c_Mobile = c.mobile
                     v.c_Address_Line1 = c.address_Line1
                     v.c_Address_Line2 = c.address_Line2
                     v.c_Address_Line3 = c.address_Line3
+                    v.total = sum
                 }
                 return v
             }
