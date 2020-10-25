0530de1857d0e2518a4e56ed1832a1441097130d
pk <pk@varan.spb.ru>
2020-10-25 23:42:53 +0300

    Populate product table - fun genProduct

--- src/com/demo1_prvt/MyModule.kt
+++ src/com/demo1_prvt/MyModule.kt
@@ -2,18 +2,44 @@ package com.demo1_prvt
 
 import com.dolmen.md.demo1_prvt.Demo1_PrvtModuleBase
 import com.dolmen.md.demo1_prvt.*
-
+import com.dolmen.serv.anno.*
 import com.dolmen.serv.conn.SelectedData
 import com.dolmen.serv.exp.Formula
 import com.dolmen.util.Text
+import java.io.File
+import java.lang.Math.random
 import java.math.BigDecimal
 import java.math.RoundingMode
+import kotlin.math.round
+import kotlin.random.Random
 
 class MyModule : Demo1_PrvtModuleBase() {
 
+    @Description("My Action")
+    @Parameters("input: String")
     fun myAction(input: String): String {
         return Text.F("\n-------------\nTest OK (input: $0)\n--------------", input)
     }
+
+    @Description("Generate products")
+    @Parameters("pathIn: String", "n: Int")
+    fun genProduct(pathIn: String, n: Int): String {
+        val fileIn = File(pathIn)
+        fileIn.useLines { lines ->
+            var i = 1
+            for (l in lines) {
+                val p = Product()
+                p.name = l.trim()
+                p.price = round((random() * 3000)) / 100
+                p.product_Type = Product.PRODUCT_TYPE.GROCERY
+                insert(p)
+                if (i == n) break
+                i++
+            }
+        }
+        return Text.F("Done")
+    }
+
 
     private fun Shipping_Order_Product.sum(): BigDecimal {
         return (this.price() * BigDecimal(this.quantity)).setScale(2, RoundingMode.HALF_UP)
