b7a6cf542ed6edbd1fa31bd8b6c48583ab3e7bda
pkvaran <pk@varan.spb.ru>
2021-03-25 12:47:36 +0300

    refactor getters

--- src/com/demo1_prvt/ShippingOrderProductFiller.kt
+++ src/com/demo1_prvt/ShippingOrderProductFiller.kt
@@ -8,8 +8,8 @@ import java.math.BigDecimal
 import java.math.BigDecimal.ZERO
 import java.math.RoundingMode
 
-class Shipping_Order_ProductFiller : Shipping_Order_Product.IShipping_Order_Product {
-    override fun getInstance(): ITableFieldFiller = Shipping_Order_ProductFiller()
+class ShippingOrderProductFiller : Shipping_Order_Product.IShipping_Order_Product {
+    override fun getInstance(): ITableFieldFiller = ShippingOrderProductFiller()
 
     private val db get() = MyModule.start()
 //    var item: Shipping_Order_Product? = null
@@ -17,18 +17,18 @@ class Shipping_Order_ProductFiller : Shipping_Order_Product.IShipping_Order_Prod
 //    private fun item(itemRec: DocTable): Doc = item ?: run { Doc(docRec, db) }
 
     override fun getI_Price(table: Shipping_Order_Product?): BigDecimal {
-        val p = db.select(Product(), table?.product)
-        val itemPrice = p.price.toBigDecimal().setScale(2, RoundingMode.HALF_UP)
-        return itemPrice
+        if (table == null) return ZERO
+        val p = db.select(Product(), table.product)
+        return p.price.toBigDecimal().setScale(2, RoundingMode.HALF_UP)
     }
 
     override fun setI_Price(table: Shipping_Order_Product?, value: BigDecimal?) {}
 
     override fun getI_Sum(table: Shipping_Order_Product?): BigDecimal {
-        val itemPrice = table?.i_Price ?: ZERO
-        val itemQnty = table?.quantity ?: 0
-        val itemSum = (itemPrice * itemQnty.toBigDecimal()).setScale(2, RoundingMode.HALF_UP)
-        return itemSum
+        if (table == null) return ZERO
+        val itemPrice = table.i_Price ?: ZERO
+        val itemQnty = table.quantity
+        return (itemPrice * itemQnty.toBigDecimal()).setScale(2, RoundingMode.HALF_UP)
     }
 
     override fun setI_Sum(table: Shipping_Order_Product?, value: BigDecimal?) {}
