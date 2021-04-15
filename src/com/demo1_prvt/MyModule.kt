8287405babe919518fa18cc4ca5998bb055c1615
pkvaran <pk@varan.spb.ru>
2021-04-15 11:56:02 +0300

    formatting

--- src/com/demo1_prvt/MyModule.kt
+++ src/com/demo1_prvt/MyModule.kt
@@ -298,8 +298,7 @@ class MyModule : Demo1_PrvtModuleBase() {
             val paidDaysAgo = (placedDaysAgo - Random.nextInt(maxPaidAfter + 1)).coerceAtLeast(0)
             val shipmentDaysAgo = placedDaysAgo - Random.nextInt(maxShipmentAfter + 1).toLong()
             //val m1 = Random.nextInt(maxCustomer)
-            val m1 = ((0.15 * rnd.nextGaussian() + 0.5) * maxCustomer).toInt()
-                    .coerceIn(0, maxCustomer - 1)
+            val m1 = ((0.15 * rnd.nextGaussian() + 0.5) * maxCustomer).toInt().coerceIn(0, maxCustomer - 1)
             o.customer = customer[m1].id
             o.datetime_Order_Placed =
                     OffsetDateTime.now().minusDays(placedDaysAgo).minusMinutes(Random.nextInt(minutesInDay).toLong())
