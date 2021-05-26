7f22945ab68126712d9fd081d44ae2a95fc8c30d
pkvaran <pk@varan.spb.ru>
2021-05-26 11:20:34 +0300

    Product url - field and screen action

--- src/com/demo1_prvt/filler/ProductFiller.kt
+++ src/com/demo1_prvt/filler/ProductFiller.kt
@@ -4,11 +4,17 @@ import com.demo1_prvt.Demo1
 import com.dolmen.md.demo1_prvt.Product
 import com.dolmen.serv.table.ITableFieldFiller
 
-class ProductFiller : Product.IProduct{
+class ProductFiller : Product.IProduct {
     private val db by lazy { Demo1.start() }
     override fun getInstance(): ITableFieldFiller = ProductFiller()
 
-    override fun getUrl(table: Product): String = "https://www.google.com/search?q=${table.name}"
+    override fun getUrl(table: Product): String {
+        val engines = listOf(
+                "https://www.google.com/search?q=",
+                "https://www.bing.com/search?q=",
+                "https://duckduckgo.com/?q=")
+        return "${engines.random()}${table.name}"
+    }
 
     override fun setUrl(table: Product, value: String?) {}
 }
