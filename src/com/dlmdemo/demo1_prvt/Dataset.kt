2d47b7a9d343a880fe38c55bf63f3754878767aa
pkvaran <pk@varan.spb.ru>
2022-08-29 12:46:00 +0300

    simpler DecorTestCard sample text field

--- src/com/dlmdemo/demo1_prvt/Dataset.kt
+++ src/com/dlmdemo/demo1_prvt/Dataset.kt
@@ -8,3 +8,8 @@ val CUSTOMER_CATEGORY_DATASET = mapOf(
     "Suspects" to null,
     "Leads" to null
 )
+
+val LOREM =
+    """Lorem ipsum dolor sit amet. Et iste excepturi est iure porro et illo sapiente cum dolorem minima? Est dolorem dolorem ut temporibus pariatur et illo exercitationem. Eum dolore sapiente aut beatae itaque non quia voluptatibus aut doloribus omnis nam minus saepe. Ad optio sapiente ut numquam eaque ut blanditiis nihil est sint iusto.
+Eos eveniet harum 33 inventore error ex reiciendis rerum ex doloribus repellendus aut nobis placeat. In quasi temporibus sed fuga corporis et officiis quod hic quia nihil et quidem iste qui autem mollitia vel magni itaque?
+At omnis nihil quo porro vero ut repellat officia ut totam autem et voluptatum laboriosam. Et perspiciatis dolor sed quod accusamus et error soluta. Sit velit aliquam et quae voluptatem ut soluta quia et dicta quos sed voluptatibus officiis a mollitia accusamus. Sapiente laborum qui dolore incidunt qui distinctio rerum vel exercitationem nihil eum voluptatem illum?""".trimIndent()
