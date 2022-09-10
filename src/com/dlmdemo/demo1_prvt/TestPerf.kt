318baf256997efe3a4490fcc974ede05e84f49e6
pkvaran <pk@varan.spb.ru>
2022-09-10 15:49:59 +0300

    testPerf

--- src/com/dlmdemo/demo1_prvt/TestPerf.kt
+++ src/com/dlmdemo/demo1_prvt/TestPerf.kt
@@ -11,9 +11,9 @@ import kotlin.system.measureTimeMillis
 class TestPerf(val m: Demo1) {
     fun testQuery(n: Int, dbUrl: String, dbUser: String, dbPass: String, dbSchema: String, fetchSize: Int) {
 
-        val t0 = measureTimeMillis { runJdbc(n, dbUrl, dbUser, dbPass, dbSchema, fetchSize) }
-        val t1 = measureTimeMillis { runSelectMap(n, fetchSize) }
-        val t2 = measureTimeMillis { runIterate(n, fetchSize) }
+        val t0 = runJdbc(n, dbUrl, dbUser, dbPass, dbSchema, fetchSize)
+        val t1 = runSelectMap(n, fetchSize)
+        val t2 = runIterate(n, fetchSize)
         val tMin = minOf(t0, t1, t2).coerceAtLeast(1)
 
         val report = "$0 (executed $n times): $1ms, $2%"
@@ -23,17 +23,19 @@ class TestPerf(val m: Demo1) {
         Txt.info(report, "iterate", t2, t2 * 100 / tMin).msg()
     }
 
-    private fun runIterate(n: Int, fetchSize: Int) {
-        repeat(n) {
-            val f = Formula.parse("", Shipping_Order_Product.T)
-            f.expectedRows = fetchSize
-            m.iterate<Shipping_Order_Product>(f) {
-                val noop = null //breakpoint
+    private fun runIterate(n: Int, fetchSize: Int): Long {
+        val f = Formula.parse("", Shipping_Order_Product.T)
+        f.expectedRows = fetchSize
+        return measureTimeMillis {
+            repeat(n) {
+                m.iterate<Shipping_Order_Product>(f) {
+                    val noop = null //breakpoint
+                }
             }
         }
     }
 
-    private fun runJdbc(n: Int, dbUrl: String, dbUser: String, dbPass: String, dbSchema: String, fetchSize: Int) {
+    private fun runJdbc(n: Int, dbUrl: String, dbUser: String, dbPass: String, dbSchema: String, fetchSize: Int): Long {
         val dbTable = "shipping_order_product__demo1_prvt"
         val q = "SELECT t.id, t.ver, t.sver, t.data FROM $dbSchema.$dbTable t order by t.id;"
         var connection: Connection? = null
@@ -42,37 +44,39 @@ class TestPerf(val m: Demo1) {
         } catch (ex: Exception) {
             Txt.error("Connection error: ${ex.message}").msg()
         }
-        if (connection == null) return
-
-        try {
-            connection.autoCommit = false
-            val query = connection.prepareStatement(q)
-            query.fetchSize = fetchSize
-            repeat(n) {
-                val result = query.executeQuery()
-                while (result.next()) {
-                    with(result) {
-                        val id = getInt("id")
-                        val ver = getInt("ver")
-                        val sver = getInt("sver")
-                        val data = getString("data")
-                        val noop = null //breakpoint
+        if (connection == null) return -1
+        return measureTimeMillis {
+            try {
+                connection.autoCommit = false
+                val query = connection.prepareStatement(q)
+                query.fetchSize = fetchSize
+                repeat(n) {
+                    val result = query.executeQuery()
+                    while (result.next()) {
+                        with(result) {
+                            val id = getInt("id")
+                            val ver = getInt("ver")
+                            val sver = getInt("sver")
+                            val data = getString("data")
+                            val noop = null //breakpoint
+                        }
                     }
                 }
+            } catch (ex: Exception) {
+                Txt.error("Query error: ${ex.message}").msg()
+            } finally {
+                connection.close()
             }
-        } catch (ex: Exception) {
-            Txt.error("Query error: ${ex.message}").msg()
-        } finally {
-            connection.close()
         }
-
     }
 
-    fun runSelectMap(n: Int, fetchSize: Int) {
+    fun runSelectMap(n: Int, fetchSize: Int): Long {
         val f = Formula.parse("", Shipping_Order_Product.T)
         f.expectedRows = fetchSize
-        repeat(n) {
-            m.selectMap(Shipping_Order_Product.fId, f)
+        return measureTimeMillis {
+            repeat(n) {
+                m.selectMap(Shipping_Order_Product.fId, f)
+            }
         }
     }
 }
